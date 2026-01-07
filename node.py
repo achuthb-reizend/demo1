e428ea2b342c23d0c6a4babb7397b85dcd8af5b6
achuthb-reizend <achuth.b@reizend.ai>
2026-01-07 22:42:07 +0530

    Create node.py

---
+++ node.py
@@ -0,0 +1,13 @@
+/**
+ * @name Manual test alert
+ * @description Test query to validate CodeQL pipeline
+ * @severity note
+ */
+
+import python
+
+from Module m
+select m, "Manual CodeQL test alert"
+
+
+what severity will it be
